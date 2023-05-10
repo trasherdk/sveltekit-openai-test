c14ed519459784b1f5aac134307090a49a021cb1
TrasherDK <trasherdk@fumlersoft.dk>
2023-05-10 07:30:33 +0200

    change adapter to node - add .env - import openai key from .env

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,13 +1,21 @@
+import { env } from '$env/dynamic/private'
+import util from 'util'
 import { OpenAI } from "langchain/llms/openai";
 
+const OPENAI_API_KEY = env.API_KEY
+console.log('API-Key:', OPENAI_API_KEY)
 
 // @ts-ignore
-export async function load() {
-  //create openai embeddings with api key 
-  const model = new OpenAI();
+export async function load () {
+  //create openai embeddings with api key
+  const model = new OpenAI({ openAIApiKey: OPENAI_API_KEY });
   const res = await model.call(
-    "What would be a good company name a company that makes colorful socks?"
+    "What is the latest version of svelte?"
   );
-  return res;
- 
+
+  console.log(util.inspect(res, true, 3, true))
+  return {
+    data: [...res]
+  }
+
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
     /** @type {import('./$types').PageServerData} */
     export let data;
 </script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
 <p>date from server: {data}</p>
